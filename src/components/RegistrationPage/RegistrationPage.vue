659eaac3becdd02d2f5b1f00b0c7709684afc67f
gabrielgatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2023-10-20 14:03:37 +0800

    Registration

---
+++ src/components/RegistrationPage/RegistrationPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <RegistrationNav /> -->
+    <RegistrationWelcome />
+  </div>
+</template>
+
+<script>
+// import RegistrationNav from './components/RegistrationNav.vue';
+import RegistrationWelcome from './components/RegistrationWelcome.vue';
+export default {
+  name: 'RegistrationPage',
+  components: {
+    // RegistrationNav,
+    RegistrationWelcome,
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/RegistrationPage/components/RegistrationNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-container>
+        <v-row>
+            <v-col cols="auto">
+                <v-img width="200" src="@/assets/Hackathon Assets/Logo Black.png"></v-img>
+            </v-col>
+        </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
